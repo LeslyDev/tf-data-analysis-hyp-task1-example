59fd5ce7484f32fca06b62bfe0196053030fff04
LeslyDev <54473665+LeslyDev@users.noreply.github.com>
2023-04-13 23:16:08 +0500

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,8 +1,9 @@
 import pandas as pd
 import numpy as np
+import scipy.stats as stats
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 623406448 # Ваш chat ID, не меняйте название переменной
 
 def solution(x_success: int, 
              x_cnt: int, 
@@ -11,4 +12,12 @@ def solution(x_success: int,
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+    control = [x_success / x_cnt]
+    test = [y_success / y_cnt]
+    alpha = 0.03
+    t_stat, p_value = stats.ttest_ind(control, test)
+    
+    if p_value < alpha:
+      return True
+    else:
+      return False
